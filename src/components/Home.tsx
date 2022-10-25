1149841b1c98e4c4af7e21780ef00550e1713669
B6332570 <111514157+B6332570@users.noreply.github.com>
2022-10-25 17:46:23 +0700

    Delete src directory

--- src/components/Home.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Home() {
-  return (
-    <div>หน้าแรกkub</div>
-  )
-}
-
-export default Home
--- src/interfaces/IComputer_os.ts
+++
@@ -1,4 +0,0 @@
-export interface Computer_ossInterface{
-    ID: number,
-    Name: string,
-}
